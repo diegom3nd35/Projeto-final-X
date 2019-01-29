8217cf2267c867d8bef2d2afeb27230583b86889
diegom3nd35 <diegom3nd35@gmai.com>
2019-01-29 02:47:14 -0300

    Adicionando slides introdutório para o app

--- Financas/app/src/main/java/com/example/diego/financas/MainActivity.java
+++ Financas/app/src/main/java/com/example/diego/financas/MainActivity.java
@@ -5,19 +5,36 @@ import android.os.Bundle;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends IntroActivity {
+
 
     private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main); --> Desativando a tela principal
 
+        //Deixando os botões invisiveis
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
 
+        //Slide 1
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.mi_icon_color_light)
+                .fragment(R.layout.slide_1)
+                .build()
+        );
+        //Slide 2
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.mi_icon_color_light)
+                .fragment(R.layout.slide_2)
+                .build()
 
+        );
+        
     }
-
-
 }
