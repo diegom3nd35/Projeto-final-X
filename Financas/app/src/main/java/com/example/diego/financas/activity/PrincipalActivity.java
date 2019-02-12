102615cfe2a219eef99839c595d186a6d6c0c315
Diego Mendes <diegom3nd35@gmail.com>
2019-02-11 23:35:36 -0300

    algumas correcções na hora de preencher os campos de receitas e depesas

--- Financas/app/src/main/java/com/example/diego/financas/activity/PrincipalActivity.java
+++ Financas/app/src/main/java/com/example/diego/financas/activity/PrincipalActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -41,6 +43,8 @@ public class PrincipalActivity extends AppCompatActivity {
     private DatabaseReference usuarioReferencia;
     private ValueEventListener valueEventListenerUsuario;
 
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,7 +55,19 @@ public class PrincipalActivity extends AppCompatActivity {
         textoSaldo = findViewById(R.id.textView_saldo);
         textoSaldacao = findViewById(R.id.textView1_saldacao);
         calendarView = findViewById(R.id.calendarView);
+        recyclerView = findViewById(R.id.recycler_movimentacao);
         configuraCalendarView();
+
+        // Configurando adapter
+
+        // configurando RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager( layoutManager);
+        recyclerView.setHasFixedSize(true);
+        
+
+
+
     }
 
     protected void onStart(){
